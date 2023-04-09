98e3beec06d3c5b3509bc6354b5d67a458e771a0
fiticostini <fiticostini@gmail.com>
2023-04-09 17:13:00 +0000

    entrega tarea

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,66 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";						
 
 //create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [tareas, setTareas] = useState([]);
+    const [tareaName, setTareaName] = useState("");
+    const handleTareaChange = (event) => {
+        setTareaName(event.target.value);
+    };
+    const handleAddEnter = (event) => {
+        console.log(event.key);
+        if (event.key === "Enter") {
+            const newTarea = {
+                accion: tareaName,
+            };
+            const newTareas = [...tareas, newTarea]
+            setTareas(newTareas);
+            setTareaName("");
+        }
+    }
+    const handleAddClick = () => {
+        
+        const newTarea = {
+            accion: tareaName,
+        };
+        const newTareas = [...tareas, newTarea]
+        setTareas(newTareas);
+        setTareaName("");
+        
+    };
+
+    const handleRemove = (id) => {
+        setTareas(tareas.filter((item, index) => index != id))
+    }
+    return (
+        <div className="d-flex justify-content-center">
+            <div className="text-center w-50">
+                <h1 className="text-center mt-5">TASK LIST</h1>
+                <div className="card m-5 ">
+                    <div className="card-header">
+                        <input type="text" placeholder="New task" onChange={(event) => handleTareaChange(event)} onKeyDown={(event) => handleAddEnter(event)} value={tareaName} />
+                        <button className="boton"  onClick={() => handleAddClick()}>Add me</button>
+                    </div>
+                    <div className="card-body">
+					{tareas.length === 0 && <p>there are no task</p>}
+                        <ul className="list-group list-group-flush ">
+                            {tareas.map((tarea, index) => {
+                                return (
+                                    <li className="list-group-item d-flex justify-content-between" key={`${tarea.accion}-${index}`}>
+                                        <p>{tarea.accion}</p>
+                                        <button className="btn-close" aria-label="Close" onClick={() => handleRemove(index)}></button>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                    <div className="card-footer">{tareas.length} item left</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
+
